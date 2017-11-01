7a04351e42713525b697f397cb5103f0aeb6b9b1
Johan Medrano <jmsnowrider@gmail.com>
2017-11-01 22:26:05 +0100

    [0] rm a.out

--- test/meta/test_spice_utils.hxx
+++ test/meta/test_spice_utils.hxx
@@ -4,7 +4,7 @@
 #include "../../include/core/all.hxx"
 #include "../../include/meta/all.hxx"
 #include <iostream>
-struct TestSpiceUtils
+class TestSpiceUtils
 {
 	template <class S>
 	struct test_is_spice : is_spice<S>
@@ -14,7 +14,7 @@ struct TestSpiceUtils
 	{
 		std::cout << std::boolalpha;
 		std::cout << "is_spice <int> = " << is_spice<int>::value << std::endl; 
-		std::cout << "is_spice <Atom> = " << is_spice<Atom<>>::value << std::endl; 
+		std::cout << "is_spice <Atom<>> = " << is_spice<Atom<>>::value << std::endl; 
 		std::cout << "is_spice <Spice> = " << is_spice<Spice>::value << std::endl; 
 		return is_spice<int>::value && is_spice<Atom<>>::value && is_spice<Spice>::value; 
 	}
