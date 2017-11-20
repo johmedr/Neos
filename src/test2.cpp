ce951a3cd158cff6743cd631d2fedb79b0025c2c
Johan Medrano <jmsnowrider@gmail.com>
2017-11-20 20:34:48 +0100

    Add new draft for module inheritence

---
+++ src/test2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <type_traits>
+ 
+struct Module
+{};
+
+template <class M1 , class ...Mn>
+// M, C 
+struct Atom : std::enable_if<std::is_base_of<Module, M1>::value, M1>::type, Atom<Mn...>
+{}; 
+
+template <class M> 
+struct Atom<M> : std::enable_if<std::is_base_of<Module, M>::value, M>::type  
+{}; 
+
+struct a_mod : Module
+{
+	static const bool a= true; 
+};
+
+struct b_mod : Module
+{
+	static const bool b = true; 
+};
+
+struct abis_mod : Module 
+{
+	static const bool a = false; 
+}; 
+
+int main() 
+{
+	std::cout << std::boolalpha; 
+	std::cout << Atom<a_mod>::a << std::endl; 
+	std::cout << Atom<b_mod>::b << std::endl; 
+	std::cout << Atom<a_mod, b_mod>::a << Atom<a_mod, b_mod>::b << std::endl;
+}
