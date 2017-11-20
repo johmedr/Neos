b635524103f1d9d4e08c1d8faeaf640943f9185b
Johan Medrano <jmsnowrider@gmail.com>
2017-11-20 18:58:26 +0100

    Improve draft in test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
+#include <cstdlib>
 
-struct Base
+#include "../include/meta/conditions.hxx"
+	
+
+struct Module
 {}; 
 
+
 template <class C>
-struct Mod : Base
+struct Mod : Module
 {};
 
 template <>
@@ -20,15 +26,49 @@ struct Mod<char>
 	void f2(char b) {}
 };
 
-template <class ...C>
-struct Derv : Base, C... // Solution 
+
+template <class ...M>
+struct Atom :  M... // Solution 
 {}; 
+
+
+struct mayia_static_mod : Module 
+{
+	struct mayia_static
+	{
+	public: 
+		static void f() 
+		{
+			std::cout << "Hello Static Mayia ! :)" << std::endl; 
+		}
+	};
+}; 
+
+
+struct mayia_dynamic_mod : Module 
+{
+	struct mayia_dynamic_conf
+	{
+	private:
+		const int nb;  
+	public: 
+		mayia_dynamic_conf() : nb(rand()) {}
+
+		void f() 
+		{
+			std::cout << "Hello Dynamic Mayia (" << nb << ") ! :)" << std::endl; 
+		}
+	};
+
+	mayia_dynamic_conf mayia_dynamic; 
+}; 
+
 
 int main() 
 {
-	Derv<Mod<char>> dc; 
-	Derv<Mod<int>> di;
-	Derv<Mod<int>, Mod<char>> dic; 
+	Atom<Mod<char>> dc; 
+	Atom<Mod<int>> di;
+	Atom<Mod<int>, Mod<char>> dic; 
 
 	dc.f1(3); 
 	dc.f2(4); 
@@ -38,4 +78,25 @@ int main()
 	dic.f1(3); 
 	dic.f2(5); 
 	dic.f(); 
+
+	using static_atom = Atom<mayia_static_mod>; 
+	static_atom::mayia_static::f(); 
+
+	Atom<mayia_dynamic_mod> dynamic_atom1, dynamic_atom2; 
+	dynamic_atom1.mayia_dynamic.f(); 
+	dynamic_atom2.mayia_dynamic.f(); 
+
+	using hybrid_atom = Atom< mayia_static_mod, mayia_dynamic_mod >; 
+	hybrid_atom at; 
+	using hybrid_atom2 = Atom< mayia_static_mod, mayia_dynamic_mod >; 
+	hybrid_atom2 at2; 
+
+	hybrid_atom::mayia_static::f(); 
+	hybrid_atom2::mayia_static::f(); 
+
+	at.mayia_dynamic.f(); 
+	at2.mayia_dynamic.f(); 
+
+	std::cout << std::boolalpha; 
+	std::cout << equals< hybrid_atom, hybrid_atom2 >::value << std::endl; 
 }
