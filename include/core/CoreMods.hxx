c84b11225f9f42c066379bb1949dda16b4a77d81
Johan Medrano <jmsnowrider@gmail.com>
2017-11-27 00:29:48 +0100

    Add separated default mod for module registry

--- include/core/CoreMods.hxx
+++ include/core/CoreMods.hxx
@@ -5,19 +5,6 @@
 
 namespace __core__
 {
-	template <class ...Ms>
-	struct __modregistry__ : Module
-	{
-		using ModuleRegistry = ModList<Ms ...>;
-
-		template <class Mod>
-		static constexpr bool __contains__ = contains<
-												Mod, 
-												ModuleRegistry
-											>::value; 
-
-	};
-
 	template <class ...Ms> 
 	struct arch : Module 
 	{
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 int main()
 {
 	Atom<> at; 
-
 	
 	std::cout << std::boolalpha;
 
